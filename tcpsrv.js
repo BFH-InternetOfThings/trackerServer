9cbd067db6d7f924c93084168027ffef38f169a3
roger <roger.jaggi@blastbeatsyndicate.com>
2014-10-24 00:42:34 +0200

    binary protocol

--- tcpsrv.js
+++ tcpsrv.js
@@ -19,13 +19,14 @@ var atDateTime = new Parser()
     .uint8('month') // 1-12
     .uint8('day'); // 1 to 31
 
+
+var atAsyncHearbeatMessage = new Parser()
+    .endianess('big')
+    .nest('rtc', { type: atDateTime }); // rtc date
+
 var atAsyncGPSMessage = new Parser()
     .endianess('big')
-    .uint32('modemID1') // Modem ID or IMEI Part 1
-    .uint32('modemID2') // Modem ID or IMEI Part 2
-    .uint16('messageID') // See Message ID Table
-    .uint16('dataLength') // 16-bit data length
-    .nest('gps', atDateTime) // gps date
+    .nest('gps', { type: atDateTime }) // gps date
     .int32('latitude') // 0.00001 degree units
     .int32('longitude') // 0.00001 degree units
     .uint16('speed') // 0.1 meters per second units
@@ -36,16 +37,30 @@ var atAsyncGPSMessage = new Parser()
     .uint16('iostatus') // see I/O Status table
     .uint8('vehicleStatus') // see Vehicle Status Table
     .uint16('analogInput1') // 0.001 voltage units
-    .unit16('analogInput2') // 0.001 voltage units
-    .nest('rtc', atDateTime)
-    .nest('posSending', atDateTime);
+    .uint16('analogInput2') // 0.001 voltage units
+    .nest('rtc', { type: atDateTime })
+    .nest('posSending', { type: atDateTime });
+
+var atAsyncStatusMessage = new Parser()
+    .endianess('big')
+    .uint32('modemID1') // Modem ID or IMEI Part 1
+    .uint32('modemID2') // Modem ID or IMEI Part 2
+    .uint16('messageID') // See Message ID Table
+    .uint16('dataLength') // 16-bit data length
+    .choice('data', {
+        tag: 'messageID',
+        choices: {
+            0xAB: atAsyncHearbeatMessage
+        },
+        defaultChoice: atAsyncGPSMessage
+    });
 
 var atAsyncTextMessage = new Parser()
     .endianess('big')
     .uint16('dataLength') // 16-bit data length
     .string('textMessage', { length: 'dataLength '})
-    .nest('rtc', atDateTime)
-    .nest('posSending', atDateTime);
+    .nest('rtc', { type: atDateTime })
+    .nest('posSending', { type: atDateTime });
 
 
 var atCommandResponse = new Parser()
@@ -55,38 +70,38 @@ var atCommandResponse = new Parser()
 
 var atASCIIAcknowledge = new Parser()
     .endianess('big')
+    .uint16('transactionID')
     .uint16('sequenceID')
     .uint32le('modemID');
 
-var atBinaryAcknowledge = new Parser()
-    .endianess('big')
-    .uint16('statusCode');
-
-var atBinaryResponsePacket = new Parser
-    .endianess('big')
-    .uint8('messageEncoding')
-    .uint8('messageType')
-    .choice('type', {
-        tag: 'messageType',
-        choices: {
-            0x00: atAsyncGPSMessage,
-            0x01: atCommandResponse, //response success
-            0x02: atAsyncTextMessage,
-            0x03: atBinaryAcknowledge,
-            0x04: atCommandResponse // response error
-        }
-    });
-
-var atPacket = new Parser()
+var atBinaryResponsePacket = new Parser()
     .endianess('big')
     .uint16('transactionID')
-    .choice('format', {
-        tag: 'transactionID',
+    .uint8('messageEncoding')
+    .uint8('messageType')
+    .choice('message', {
+        tag: 'messageEncoding',
         choices: {
-            0xfaf8: atASCIIAcknowledge
-        },
-        defaultChoice: atBinaryResponsePacket
+            0x00: atAsyncStatusMessage,
+            0x01: atCommandResponse,
+            0x02: atAsyncTextMessage,
+            0x03: atAsyncTextMessage,
+            0x04: atAsyncTextMessage
+        }
     });
+    /* .choice('message', {
+        tag: 'messageEncodingAndType',
+        choices: {
+            0x0002: atAsyncStatusMessage,
+            0x0100: atCommandResponse, // request
+            0x0101: atCommandResponse, // response success
+            0x0104: atCommandResponse, // response error
+            0x0202: atAsyncTextMessage,
+            0x0302: atAsyncTextMessage,
+            0x0402: atAsyncTextMessage,
+            0x0003: atBinaryAcknowledge
+        }
+    }); */
 
 // Keep track of the chat clients
 var clients = [];
@@ -102,7 +117,12 @@ function broadcast(message) {
     clients.forEach(function (client) {
         // only send if inital Handshake is done, otherwise the tracker won't answer the query
         if (client.isTrackerHandshakeDone == true) {
-            client.write(message);
+            if(client.isASCIIFormat == true) {
+                client.write(message);
+            }
+            else {
+                client.sendBinaryCommand(1, message);
+            }
         }
     });
 }
@@ -112,7 +132,8 @@ net.createServer(function (socket) {
 
     // Identify this client
     socket.name = socket.remoteAddress + ":" + socket.remotePort;
-    socket.isTrackerHandshakeDone = false;
+    socket.isInitHandshakeDone = false;
+    socket.isASCIIFormat = false;
     socket.currentCommand = null;
     socket.commandQueue = [];
 
@@ -120,14 +141,84 @@ net.createServer(function (socket) {
     clients.push(socket);
     console.log('client ' + socket.name + ' connected!');
 
+    socket.sendBinaryAcknowledge = function(transactionID, success) {
+        var ackBuffer = new Buffer(6);
+        ackBuffer.writeUInt16BE(transactionID, 0);
+        ackBuffer.writeUInt8(0x00, 2);
+        ackBuffer.writeUInt8(0x03, 3);
+        ackBuffer.writeUInt16BE( ( success ? 0x0000 : 0x0001 ), 4);
+
+        socket.write(ackBuffer);
+    };
+
+    socket.sendBinaryCommand = function(transactionID, commandstring) {
+        var buf = new Buffer(6 + commandstring.length);
+        buf.writeUInt16BE(transactionID, 0);
+        buf.writeUInt8(0x01, 2);
+        buf.writeUInt8(0x00, 3);
+        buf.writeUInt16BE( commandstring.length, 4);
+        buf.write( commandstring, 6, commandstring.length, 'ascii');
+
+        console.log(buf);
+
+        socket.write(buf);
+    };
+
     // Handle incoming messages from clients.
     socket.on('data', function (data) {
 
-        var packet = atPacket(data);
+        if(socket.isASCIIFormat) {
+            process.stdout.write(data);
 
-        console.log(data);
+            // prompt for a new command for the tracker
+            rl.prompt();
+            return;
+        }
+        else if(data.readUInt16BE(0) == 0xfaf8) {
+
+            var asciiAck = atASCIIAcknowledge.parse(data);
+
+            // answer handshake
+            socket.write(data);
+
+            socket.isInitHandshakeDone = true;
+            socket.isASCIIFormat = true;
+            console.log("Heartbeat no. " + asciiAck.sequenceID + " from modem id " + asciiAck.modemID + " received!");
+            return;
+        }
+        else {
+            socket.isASCIIFormat = false;
+        }
+
+
+        // Binary format
+
+        //console.log(data);
+        try {
+            var packet = atBinaryResponsePacket.parse(data);
+        }
+        catch(err) {
+            console.log(err);
+
+            // binary data analysis failed, check if we use the correct format
+            //socket.write('at$format?');
+
+            return
+        }
+
         console.log(packet);
 
+        // Check for async answer
+        if(packet.messageType == 0x02) {
+            // answer async message with acknowledge
+            socket.sendBinaryAcknowledge(packet.transactionID, true);
+        }
+
+        rl.prompt();
+
+
+
+        /*
         if( data.length == 8 ) { // acknowledge message
             ackPacket = atFormatASCIIAcknowledge.parse(data);
             //console.log(data);
@@ -138,7 +229,7 @@ net.createServer(function (socket) {
                 console.log("Heartbeat no. " + ackPacket.sequenceID + " from modem id " + ackPacket.modemID + " received!");
                 return;
             }
-        }
+        } */
 
         // check for async GPS Position message
         // format:
@@ -149,8 +240,7 @@ net.createServer(function (socket) {
 
 
 
-        // prompt for a new command for the tracker
-        rl.prompt();
+
     });
 
     // Remove the client from the list when it leaves
@@ -167,9 +257,13 @@ console.log("Tissan Tracker server running at port 9090\n");
 
 
 rl.on('line', function(line) {
+
+    console.log(line);
+
     if (line === "exit") {
         rl.close();
     }
+
     broadcast(line);
 
 }).on('close',function(){
