ebf01184f90ee2deb5bd88409c9c09f355c695ae
roger <roger.jaggi@blastbeatsyndicate.com>
2014-11-21 12:52:15 +0100

    better refresh implementation

--- public/javascripts/smoje.js
+++ public/javascripts/smoje.js
@@ -1,6 +1,8 @@
 /**
  * Created by roger on 11/16/14.
  */
+var markerList = [];
+
 function initialize() {
     //			var image = {
     //				url: 'http://demo.symphonythemes.com/drupal7/bizreview/sites/demo.symphonythemes.com.drupal7.bizreview/themes/bizreview/amarkers/green.png',
@@ -29,59 +31,78 @@ function initialize() {
 
     var infowindow = new google.maps.InfoWindow();
 
-    $.ajax({
-        dataType: "json",
-        url: "/smoje-api/v1/trackerList",
-        success: function(data) {
 
-            for(var i = 0; i < data.length; i++)
-            {
-                var tracker = data[i];
+    var updateMarkers = function() {
+        $.ajax({
+            dataType: "json",
+            url: "/smoje-api/v1/trackerList",
+            success: function (data) {
+                // clear all markerList
+                var i;
 
-                var trackerPoint =  new google.maps.LatLng(tracker.lastPosition.latitude , tracker.lastPosition.longitude);
+                for (i = 0; i < markerList.length; i++) {
+                    markerList[i].setMap(null);
+                }
+
+                markerList = [];
+
+                for (i = 0; i < data.length; i++) {
+                    var tracker = data[i];
+
+                    var trackerPoint = new google.maps.LatLng(tracker.lastPosition.latitude, tracker.lastPosition.longitude);
 
 
+                    var marker = new google.maps.Marker({
+                        position: trackerPoint,
+                        icon: image,
+                        map: map,
+                        title: 'Tracker ' + tracker.deviceID
+                    });
+                    markerList.push(marker);
 
-                var marker = new google.maps.Marker({
-                    position: trackerPoint,
-                    icon: image,
-                    map: map,
-                    title: 'Tracker ' + tracker.deviceID
-                });
+                    google.maps.event.addListener(marker, 'click', function () {
 
-                google.maps.event.addListener(marker, 'click', function() {
+                        $.ajax({
+                            dataType: "json",
+                            url: "/smoje-api/v1/trackerList",
+                            success: function (data2) {
 
-                    $.ajax({
-                        dataType: "json",
-                        url: "/smoje-api/v1/trackerList",
-                        success: function (data2) {
+                                for (var i = 0; i < data2.length; i++) {
+                                    if (data2[i].deviceID === tracker.deviceID) {
+                                        var tracker2 = data2[i];
+                                        var contentString = '<div id="mapContent">' +
+                                            '<h1 class="mapHeading">Tracker ' + tracker2.deviceID + '</h1>' +
+                                            '<div id="mapContent">' +
+                                            '<p><b>Last update:</b><br />' + moment(tracker2.timeUpdated).format("LLL") + '</p> ' +
+                                            '<p><b>Status:</b> ' + ( tracker2.status === "a" ? "Connected" : "Disconnected" ) + '</p> ' +
+                                            '<p><b>Position:</b> ' + tracker2.lastPosition.latitude + ' / ' + tracker2.lastPosition.longitude + '</p> ' +
+                                            '<p><b>Battery Voltage:</b> ' + tracker2.lastBatteryVoltage + 'V</p> ' +
+                                            '<p><b>Extern Voltage:</b> ' + tracker2.lastExternVoltage + 'V</p> ' +
+                                            '</div>' +
+                                            '</div>';
 
-                            for (var i = 0; i < data2.length; i++) {
-                                if (data2[i].deviceID === tracker.deviceID) {
-                                    var tracker2 = data2[i];
-                                    var contentString = '<div id="mapContent">'+
-                                        '<h1 class="mapHeading">Tracker ' + tracker2.deviceID + '</h1>'+
-                                        '<div id="mapContent">'+
-                                        '<p><b>Last update:</b><br />' + moment(tracker2.timeUpdated).format("LLL")  + '</p> ' +
-                                        '<p><b>Status:</b> ' + ( tracker2.status === "a" ? "Connected" : "Disconnected" ) + '</p> ' +
-                                        '<p><b>Position:</b> ' + tracker2.lastPosition.latitude + ' / ' + tracker2.lastPosition.longitude + '</p> ' +
-                                        '<p><b>Battery Voltage:</b> ' + tracker2.lastBatteryVoltage + 'V</p> ' +
-                                        '<p><b>Extern Voltage:</b> ' + tracker2.lastExternVoltage + 'V</p> ' +
-                                        '</div>'+
-                                        '</div>';
+                                        var trackerPoint2 = new google.maps.LatLng(tracker2.lastPosition.latitude, tracker2.lastPosition.longitude)
 
-                                    infowindow.setContent(contentString);
-                                    infowindow.close();
-                                    infowindow.open(map, marker);
-                                    return;
+                                        marker.setPosition(trackerPoint2);
+                                        //map.setCenter(trackerPoint2);
+                                        //map.setZoom(14);
+
+                                        infowindow.setContent(contentString);
+                                        infowindow.close();
+                                        infowindow.open(map, marker);
+                                        return;
+                                    }
                                 }
                             }
-                        }
+                        });
                     });
-                });
+                }
             }
-        }
-    });
+        });
+    };
+    updateMarkers();
+    setInterval(updateMarkers, 15000);
+
 
     //open Smoje1 infowindow at default on startsite
     //infowindow.setContent(contentString1);
