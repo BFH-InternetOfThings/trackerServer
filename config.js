c44cbe30653df1f609ce8fef1abd36089146d7b3
roger <roger.jaggi@blastbeatsyndicate.com>
2014-12-08 23:58:32 +0100

    the config

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ exports.port = process.env.PORT || 3000;
 exports.trackerport = process.env.PORT || 9090;
 
 exports.mongodb = {
-    uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost/tracker'
+    uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/tracker'
 };
 exports.companyName = 'Roger Jaggi';
 exports.projectName = 'Smoje Tracker Server';
