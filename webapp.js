21ee520383692d456efa8051807e827abc697381
roger <roger.jaggi@blastbeatsyndicate.com>
2014-12-09 20:52:59 +0100

    reenabled access-control

--- webapp.js
+++ webapp.js
@@ -60,7 +60,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-//app.use(allowCrossDomain);
+app.use(allowCrossDomain);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
